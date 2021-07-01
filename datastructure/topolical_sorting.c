0a0b6a75b1e79e3ed3f2ce98f20f0c7cb8560d1d
Mohammed Rasik <rasikmhd10@gmail.com>
2021-07-01 12:08:09 +0530

    programs

---
+++ datastructure/topolical_sorting.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+ 
+int main(){
+int i,j,k,n,a[10][10],indeg[10],flag[10],count=0;
+char arr1[] = { 'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
+ 
+printf("Input the no of vertices:\n");
+scanf("%d",&n);
+printf("\n");
+
+printf("Enter the adjacency matrix values:\n");
+for(i=0;i<n;i++){
+printf("Enter data for row %d\n : ",i+1);
+for(j=0;j<n;j++)
+scanf("%d",&a[i][j]);
+}
+ 
+for(i=0;i<n;i++){
+        indeg[i]=0;
+        flag[i]=0;
+    }
+ 
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            indeg[i]=indeg[i]+a[j][i];
+ 
+    printf("\nThe topological order is:  ");
+ 
+    while(count<n){
+        for(k=0;k<n;k++){
+            if((indeg[k]==0) && (flag[k]==0)){
+                printf("%c\t",arr1[k]);
+                flag [k]=1;
+            }
+ 
+            for(i=0;i<n;i++){
+                if(a[i][k]==1)
+                    indeg[k]--;
+            }
+        }
+ 
+        count++;
+    }
+    
+    return 0;
+}
